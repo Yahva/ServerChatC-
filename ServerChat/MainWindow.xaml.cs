91d472ffc02c25746e9c09d851eb901e9f24f020
Ivan K <vania.ru.ru@mail.ru>
2019-02-22 20:51:41 +0300

    Добавьте файлы проекта.

---
+++ ServerChat/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ServerChat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int portHost = 5555; // порт для приема входящих запросов
+        private ServerPart server; // сервер
+        private Thread listenThread; // потока для прослушивания
+
+        private static ObservableCollection<string> _listClients;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _listClients = new ObservableCollection<string>();
+            listBoxListClients.ItemsSource = _listClients;
+
+            textBoxPortServer.Text = portHost.ToString();
+        }
+
+
+        public void RunServer(int portHost)
+        {
+            try
+            {
+                server = new ServerPart(this, portHost);
+                listenThread = new Thread(new ThreadStart(server.Listen));
+                listenThread.Start(); //старт потока
+            }
+            catch (Exception ex)
+            {
+                server.Disconnect();
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public void AddClientInList(string nameClient)
+        {
+            // Получить диспетчер от текущего окна и использовать его для вызова кода обновления
+            this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate ()
+                {
+                    _listClients.Add(nameClient);
+                }
+            );
+        }
+
+        public void RemoveClientWithList(string nameClient)
+        {
+            // Получить диспетчер от текущего окна и использовать его для вызова кода обновления
+            this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate ()
+                {
+                    _listClients.Remove(nameClient);
+                }
+            );
+        }
+
+        private void RunServer_Click(object sender, RoutedEventArgs e)
+        {
+            RunServer(portHost);
+        }
+
+        //Вывод сообщения на экран
+        public void SendToListReciveMessage(string message)
+        {
+            // Получить диспетчер от текущего окна и использовать его для вызова кода обновления
+            this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate ()
+                {
+                    TextBlock textBlock = new TextBlock();
+                    textBlock.Text = message;
+
+                    textBlock.Style = (Style)this.TryFindResource("StyleReciveMessage");
+                    spListReciveMessage.Children.Add(textBlock);
+                }
+              );
+
+        }
+
+        private void ChangePortServer_Click(object sender, RoutedEventArgs e)
+        {
+            string patternPort = @"\d{4}\d?$";
+            if(Regex.IsMatch(textBoxPortServer.Text, patternPort))
+            {
+                ((MainWindow)this.Owner).portHost = Convert.ToInt32(textBoxPortServer.Text);
+            }
+            else
+            {
+                textBoxPortServer.Text = ((MainWindow)this.Owner).portHost.ToString();
+            }
+        }
+
+       
+
+        public void ChangeStatusServer(bool IsConnect)
+        {
+            this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate ()
+                {
+                    if (IsConnect)
+                        ellipseStatusConnection.Fill = new SolidColorBrush(Colors.Green);
+                    else
+                        ellipseStatusConnection.Fill = new SolidColorBrush(Colors.Red);
+                }
+             );
+        }
+
+        private void ClosingProgram(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (server != null)
+            {
+                //communicationWithServer.SendMessageToServer("disconnect");
+                server.Disconnect();
+            }
+        }
+    }
+}
---
+++ ServerChat/ServerPart.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerChat
+{
+    class ServerPart
+    {
+        static TcpListener tcpListener; // сервер для прослушивания
+        List<ClientPart> clients = new List<ClientPart>(); // Все подключенные клиенты
+        public MainWindow mainWindow;
+        static object locker = new object();
+        private int portHost = 7777;
+
+        public ServerPart(MainWindow mainWindow, int portHost)
+        {
+            this.mainWindow = mainWindow;
+            this.portHost = portHost;
+        }
+
+        protected internal void AddConnection(ClientPart ClientPart)
+        {
+            clients.Add(ClientPart);
+        }
+        protected internal void RemoveConnection(string id)
+        {
+            // получаем по id закрытое подключение
+            ClientPart client = clients.FirstOrDefault(c => c.Id == id);
+            // и удаляем его из списка подключений
+            if (client != null)
+            {
+                clients.Remove(client);
+            }
+        }
+        // прослушивание входящих подключений
+        protected internal void Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, portHost);
+                tcpListener.Start();
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+                mainWindow.ChangeStatusServer(true);
+
+                while (true)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+                    ClientPart ClientPart = new ClientPart(tcpClient, this);
+                    Task clientThread = new Task(ClientPart.Process);
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Disconnect();
+            }
+        }
+
+        // трансляция сообщения подключенным клиентам
+        protected internal void BroadcastMessage(string message, string id)
+        {
+            Monitor.Enter(locker);
+
+            mainWindow.SendToListReciveMessage(message);
+
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            for (int i = 0; i < clients.Count; i++)
+            {
+                if (clients[i].Id != id) // если id клиента не равно id отправляющего
+                {
+                    clients[i].Stream.Write(data, 0, data.Length); //передача данных
+                }
+            }
+            Monitor.Exit(locker);
+        }
+        // отключение всех клиентов
+        protected internal void Disconnect()
+        {
+            mainWindow.ChangeStatusServer(false);
+            BroadcastMessage("disconnect", "");
+            tcpListener.Stop(); //остановка сервера
+
+            for (int i = 0; i < clients.Count; i++)
+            {
+                clients[i].Close(); //отключение клиента
+            }
+            Environment.Exit(0); //завершение процесса
+        }
+    }
+}
